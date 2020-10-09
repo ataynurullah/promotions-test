b4b9ee559483f03724a1aab9025ad57d3978dd23
nurullahatay <nurullah.atay@inomera.com>
2020-10-09 17:35:47 +0300

    Add end point

--- src/main/java/com/inomera/promotionstest/service/PromotionApplyService.java
+++ src/main/java/com/inomera/promotionstest/service/PromotionApplyService.java
@@ -9,14 +9,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Service;
 
 import java.net.MalformedURLException;
-import java.net.URL;
 
 @Service
 @Slf4j
@@ -24,14 +22,10 @@ public class PromotionApplyService {
 
     public Promotion applyPromotion(Promotion promotion) throws MalformedURLException {
         log.error("Promotion : " + promotion.getProductId() + " - " + promotion.getPayQty()+ " - " + promotion.getPromotionName());
-        //System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "heroku/google-chrome");
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--headless");
-        chromeOptions.setCapability("browserVersion", "67");
-        chromeOptions.setCapability("platformName", "Linux");
-        WebDriver driver = new RemoteWebDriver(new URL("https://promotionstest.herokuapp.com/"), chromeOptions);
-        driver.get("http://www.google.com");
-
+        WebDriver driver = new ChromeDriver(chromeOptions);
 
         openProductPage(driver, promotion);
         incrementToCart(driver, promotion);
